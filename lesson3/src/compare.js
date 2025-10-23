9483918d35c6a6d198bb8d0e57eb57b7779f7dfe
Anna Kiselyova <annyshkaavk@gmail.com>
2025-10-23 23:01:08 +0200

    push lesson 3 to Master

---
+++ lesson3/src/compare.js
@@ -0,0 +1,40 @@
+const x = 2;
+const y = '2';
+const z = true;
+
+console.log(x == y);
+console.log(x == z);
+console.log(x !== y);
+console.log(x !== z);
+console.log('----з приведення типів----');
+console.log(x === y);
+console.log(x === z);
+console.log(x !== y);
+console.log(x !== z);
+
+console.log (x == y);
+console.log (x == z);
+console.log (x != y);
+console.log (x != z);
+
+let a = 10;
+let b = '7';
+const c = false;
+console.log('----порівняння----');
+
+console.log(a > b);
+console.log(a > c);
+console.log(a < b);
+console.log(a < c);
+console.log(a >= b);
+console.log(a >= c);
+console.log(a <= b);
+console.log(a <= c);
+
+console.log('----порівняння рядків----');
+a = 'cat 2';
+b = 'cat 1';
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
